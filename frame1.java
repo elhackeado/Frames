00e1b98cf3fbff6eb00a1525e06960088f239bef
El Hackeado <theelhackeado@gmail.com>
2017-06-26 21:09:36 +0530

    Simple Layout Of Frame

---
+++ frame1.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+class frame1
+{
+	public static void main(String args[])
+	{
+		Frame f1=new Frame("This is Title");
+		f1.setVisible(true);
+		f1.setLocation(100,100);
+		f1.setSize(600,600);
+	}
+}
